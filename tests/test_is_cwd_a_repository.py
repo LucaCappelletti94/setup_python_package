0afbf9d431a12322d9ae9cae06a70d399e60d66c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-17 22:31:52 +0100

    Fixed test

--- tests/test_is_cwd_a_repository.py
+++ tests/test_is_cwd_a_repository.py
@@ -1,4 +1,4 @@
-from setup_python_package.utils import is_cwd_a_repository
+from setup_python_package.environment import is_cwd_a_repository
 
 def test_is_cwd_a_repository():
     assert is_cwd_a_repository()
