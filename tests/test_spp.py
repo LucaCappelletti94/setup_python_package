f5ce34d8f6b0c3d4043593f5749351f5008755a4
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-18 09:32:34 +0100

    [SPP] Created a backup.

--- tests/test_spp.py
+++ tests/test_spp.py
@@ -1,7 +1,7 @@
 from setup_python_package import setup_python_package
 
 def monkeypatch_input(*args, **kwargs):
-    print(args, kwargs)
+    return "\n"
 
 def test_spp(monkeypatch):
     monkeypatch.setattr('builtins.input', monkeypatch_input)
