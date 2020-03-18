4b410238cb0c2cce9e52b6ee1b6adf6efffe55b9
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-18 13:26:08 +0100

    Hopefully fixed tests

--- tests/test_build_version.py
+++ tests/test_build_version.py
@@ -1,5 +1,5 @@
 from setup_python_package.builders import build_version
-from setup_python_package.queries import get_package_name
+from setup_python_package.queries import get_package_name, get_package_version
 from .utils import clone_test_repo, delete_test_repo
 
 
@@ -7,7 +7,8 @@ def test_build_version(monkeypatch):
     clone_test_repo()
     monkeypatch.setattr('builtins.input', lambda x: "\n")
     package = get_package_name()
-    build_version(package)
+    version = get_package_version()
+    build_version(package, version)
     assert open(f"{package}/__version__.py", "r").read() == open(
         f"../expected/{package}/__version__.py", "r").read()
     delete_test_repo()
