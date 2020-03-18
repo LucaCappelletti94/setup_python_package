2eff719071d686286f2da4d7390f9b08b72f3d96
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-18 11:45:50 +0100

    Extended testing

--- tests/test_build_gitignore.py
+++ tests/test_build_gitignore.py
@@ -1,4 +1,4 @@
-from setup_python_package.builders import build_gitignore, build_init
+from setup_python_package.builders import build_gitignore, build_init, build_tests
 from setup_python_package.builders.build_manifest import build_manifest
 from .utils import clone_test_repo, delete_test_repo
 
@@ -6,7 +6,9 @@ from .utils import clone_test_repo, delete_test_repo
 def test_build_gitignore():
     clone_test_repo()
     build_gitignore()
-    build_init("setup_python_package_test_reporitory")
+    package = "setup_python_package_test_reporitory"
+    build_init(package)
     build_manifest()
+    build_tests(package)
     assert open(".gitignore", "r").read() == open("../expected/.gitignore", "r").read()
     delete_test_repo()
--- tests/test_url_exists.py
+++ tests/test_url_exists.py
@@ -1,5 +1,5 @@
 from setup_python_package.utils.url_exists import url_exists
 
 def test_url_exists():
-    assert url_exists("https://www.google.com/")
+    assert url_exists("https://github.com/LucaCappelletti94/setup_python_package")
     assert not url_exists("https://www.gooooogwfekwejfgkwegfjwhegfwghejgewkhwegkfwekwegfhogle.com/")
