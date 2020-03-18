ea71a873ad68a7f5d691a3e28747e505d07b850b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-18 12:11:37 +0100

    Extended testing with much pain

---
+++ tests/test_build_version.py
@@ -0,0 +1,12 @@
+from setup_python_package.builders import build_version
+from setup_python_package.queries import get_package_name
+from .utils import clone_test_repo, delete_test_repo
+
+
+def test_build_version(monkeypatch):
+    clone_test_repo()
+    monkeypatch.setattr('builtins.input', lambda x: "\n")
+    package = get_package_name()
+    build_version(package)
+    #assert open(f"{package}/__version__.py", "r").read() == open(f"../expected/{package}/__version__.py", "r").read()
+    delete_test_repo() 
