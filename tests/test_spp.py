ac50e8402d7991496c8d04c7e1c041c5df9f0ead
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-18 10:06:43 +0100

    Removed spp

--- tests/test_spp.py
+++
@@ -1,8 +0,0 @@
-from setup_python_package import setup_python_package
-
-def monkeypatch_input(*args, **kwargs):
-    return "\n"
-
-def test_spp(monkeypatch):
-    monkeypatch.setattr('builtins.input', monkeypatch_input)
-    setup_python_package()
