4fa2f5deaf75bbfa589cdf739fdad9b332f02725
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-20 17:45:33 +0100

    Fixed package version label

--- setup_python_package/queries/get_package_version.py
+++ setup_python_package/queries/get_package_version.py
@@ -6,7 +6,7 @@ def get_package_version() -> str:
     """Return the package version to be used."""
     return userinput(
         name="_version",
-        label="Enter the python version to use.",
+        label="Enter the package version to use",
         default=get_default_package_version(),
         validator="version_code",
         cache=False,
