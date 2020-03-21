7740f0524d6930784f2007a7f9c7daa4fc3ddd8e
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-21 17:08:26 +0100

    Extended testing

--- tests/test_spp_crash.py
+++ tests/test_spp_crash.py
@@ -1,16 +1,30 @@
 from setup_python_package import setup_python_package
 from .utils import clone_test_repo, delete_test_repo
 import os
+import pytest
 
-
-def crash(label):
+def crash1(label):
     raise KeyboardInterrupt()
 
-def test_setup_python_package(monkeypatch):
+def crash2(label):
+    raise ValueError()
+
+def test_crash1(monkeypatch):
     clone_test_repo()
     files1 = len(os.listdir())
-    monkeypatch.setattr('builtins.input', crash)
+    monkeypatch.setattr('builtins.input', crash1)
     setup_python_package()
+    files2 = len(os.listdir())
+    delete_test_repo()
+    assert files2 == files1
+
+
+def test_crash2(monkeypatch):
+    clone_test_repo()
+    files1 = len(os.listdir())
+    monkeypatch.setattr('builtins.input', crash2)
+    with pytest.raises(ValueError):
+        setup_python_package()
     files2 = len(os.listdir())
     delete_test_repo()
     assert files2 == files1
