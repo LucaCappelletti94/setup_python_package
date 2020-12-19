90195564f585dd8244f98da6f92774f9064d1f1d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-19 09:03:59 +0100

    Fixed corner case when retrieving metadata from Sonar

--- setup_python_package/utils/sonar_project_exists.py
+++ setup_python_package/utils/sonar_project_exists.py
@@ -3,8 +3,11 @@ import requests
 
 def sonar_project_exists(project_key: str) -> bool:
     """Return boolean representing if given sonar project exists."""
-    return "project not found" not in requests.get(
-        "https://sonarcloud.io/api/project_badges/measure?project={project_key}&metric=coverage".format(
-            project_key=project_key
-        )
-    ).text.lower()
+    try:
+        return "project not found" not in requests.get(
+            "https://sonarcloud.io/api/project_badges/measure?project={project_key}&metric=coverage".format(
+                project_key=project_key
+            )
+        ).text.lower()
+    except requests.exceptions.ConnectionError:
+        return False
--- tests/test_sonar_project_exists.py
+++ tests/test_sonar_project_exists.py
@@ -3,3 +3,4 @@ from setup_python_package.utils import sonar_project_exists
 
 def test_sonar_project_exists():
     assert sonar_project_exists("LucaCappelletti94_setup_python_package")
+    assert not sonar_project_exists("kdjhdgfkwjhfgkjhekjhwegfkjhwe")
